955bd295572d7708ddee14a118d9cab2b08fcd85
ashisranjan116-lab <ashisranjan116@gmail.com>
2026-07-09 15:01:40 +0530

    feat: add cloudinary file provider

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,8 +12,6 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
-      
-      
     }
   },
   // This absolutely guarantees the admin is disabled on Render
@@ -32,6 +30,24 @@ module.exports = defineConfig({
       resolve: '@medusajs/medusa/event-bus-redis',
       options: {
         redisUrl: process.env.REDIS_URL,
+      },
+    },
+    // CLOUDINARY CONFIGURATION ADDED BELOW
+    {
+      resolve: "@medusajs/file",
+      options: {
+        providers: [
+          {
+            resolve: "medusa-file-cloudinary",
+            id: "cloudinary",
+            options: {
+              cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+              api_key: process.env.CLOUDINARY_API_KEY,
+              api_secret: process.env.CLOUDINARY_API_SECRET,
+              secure: true,
+            },
+          },
+        ],
       },
     },
   ],
