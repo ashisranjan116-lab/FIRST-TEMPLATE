10ae82dee8c3add24132ba5f89f4a214a06c79aa
ashisranjan116-lab <ashisranjan116@gmail.com>
2026-07-09 21:01:22 +0530

    chore: remove cloudinary dependency from package.json

--- medusa-config.ts
+++ medusa-config.ts
@@ -32,23 +32,5 @@ module.exports = defineConfig({
         redisUrl: process.env.REDIS_URL,
       },
     },
-    // CLOUDINARY CONFIGURATION ADDED BELOW
-    {
-      resolve: "@medusajs/file",
-      options: {
-        providers: [
-          {
-            resolve: "@tsc_tech/medusa-plugin-cloudinary",
-            id: "cloudinary",
-            options: {
-              cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-              api_key: process.env.CLOUDINARY_API_KEY,
-              api_secret: process.env.CLOUDINARY_API_SECRET,
-              secure: true,
-            },
-          },
-        ],
-      },
-    },
   ],
 })
